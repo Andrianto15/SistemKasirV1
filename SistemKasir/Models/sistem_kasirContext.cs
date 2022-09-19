3b15e7aef0fc5197b51d632f8faa491becb666d1
Andrianto <nurekaiskandar15@gmail.com>
2022-09-19 14:20:48 +0700

    Minor update connection string

--- SistemKasir/Models/sistem_kasirContext.cs
+++ SistemKasir/Models/sistem_kasirContext.cs
@@ -28,7 +28,7 @@ namespace SistemKasir.Models
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-U34VDN2H;Database=sistem_kasir;Trusted_Connection=False;User ID=executor;Password=executor123;");
+                optionsBuilder.UseSqlServer("Server=(local);Database=sistem_kasir;Trusted_Connection=False;User ID=executor;Password=executor123;");
             }
         }
 
